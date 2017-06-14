4d2a7205c8b72a472f597c3193f74fa804531a2e
Mike Kafka <daylife.mike@gmail.com>
2017-06-14 14:52:35 -0400

    gets gallery groups working

--- photomosaic-lightbox-bridge-ilightbox.js
+++ photomosaic-lightbox-bridge-ilightbox.js
@@ -4,6 +4,7 @@
     window.PhotoMosaic.LightboxBridge.ilightbox = function ($, $mosaic, $items) {
         if (window.ILIGHTBOX && $.fn.iLightBox) {
             var options = window.ILIGHTBOX.options && eval("(" + window.ILIGHTBOX.options + ")") || {};
+            var rel = 'ilightbox[' + $mosaic.attr('id') + ']';
 
             if ($mosaic.not('.ilightbox_initialized')) {
                 $mosaic.addClass('ilightbox_gallery ilightbox_initialized')
@@ -11,9 +12,10 @@
                     var $item = $(this);
                     $item
                         .attr('source', $item.attr('href'))
-                        .attr('data-caption', $item.find('img').attr('title'));
-                    window.ILIGHTBOX.instances.push($item.iLightBox(options));
+                        .attr('data-caption', $item.find('img').attr('title'))
+                        .attr('rel', rel);
                 });
+                window.ILIGHTBOX.instances.push($('[rel="' + rel + '"]').iLightBox(options));
             }
         }
     }
