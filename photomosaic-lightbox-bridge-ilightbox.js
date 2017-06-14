32767379f8af5608825a50dc20b754d0bea7f694
Mike Kafka <daylife.mike@gmail.com>
2017-06-14 05:00:30 -0400

    gets single images working (not gallery groups)

--- photomosaic-lightbox-bridge-ilightbox.js
+++ photomosaic-lightbox-bridge-ilightbox.js
@@ -2,6 +2,19 @@
     'use strict';
 
     window.PhotoMosaic.LightboxBridge.ilightbox = function ($, $mosaic, $items) {
-        console.log( '-- ilightbox --' );
+        if (window.ILIGHTBOX && $.fn.iLightBox) {
+            var options = window.ILIGHTBOX.options && eval("(" + window.ILIGHTBOX.options + ")") || {};
+
+            if ($mosaic.not('.ilightbox_initialized')) {
+                $mosaic.addClass('ilightbox_gallery ilightbox_initialized')
+                $items.each(function(){
+                    var $item = $(this);
+                    $item
+                        .attr('source', $item.attr('href'))
+                        .attr('data-caption', $item.find('img').attr('title'));
+                    window.ILIGHTBOX.instances.push($item.iLightBox(options));
+                });
+            }
+        }
     }
 }(window));
